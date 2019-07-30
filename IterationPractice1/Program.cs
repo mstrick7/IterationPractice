5c26d3aca944927e8a7981df7c1b3f739fbe8000
mstrick7 <32557695+mstrick7@users.noreply.github.com>
2019-07-30 17:55:02 -0500

    Initial Commit. Used git version control on create

---
+++ IterationPractice1/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+
+namespace IterationPractice
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			//for (int i = 0; i < 10; i++)
+			//{
+			//    if (i % 2 == 0)
+			//    {
+			//        Console.WriteLine(i + "is divisible by 2");
+			//    }
+			//    else
+			//    {
+			//        Console.WriteLine(i + "is not divisible by 2");
+			//    }
+			//}
+
+			//String hello = "Hello";
+
+			//for (int i = 0;i < hello.Length; i++)
+			//{
+			//    if (hello[i] == 'e')
+			//    {
+			//        Console.WriteLine(hello[i]);
+			//    }
+			//}
+
+			//foreach (var letter in hello)
+			//{
+			//    if (letter == 'e')
+			//    {
+			//        Console.WriteLine("the letter is: "+ letter);
+			//    }
+			//}
+
+			//var i = 0;
+			//while (i < 10)
+			//{
+			//    if (i % 2 == 0)
+			//    {
+			//        Console.WriteLine(i + "is divisible by 2");
+			//    }
+			//    else
+			//    {
+			//        Console.WriteLine(i + "is not divisible by 2");
+			//    }
+			//    i++;
+			//}
+
+			//    var i = 0;
+			//    do
+			//    {
+			//        Console.WriteLine(i * 10);
+			//        i++;
+			//    }
+			//    while (i < 10);
+
+			//}
+		}
+	}
+}
